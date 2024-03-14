577640e3010f9c5680123ebe17115d4191d6b021
wegorz9 <rpawer2@gmail.com>
2024-03-14 13:20:58 +0100

    interfaces and implements of those

---
+++ src/Krystian.java
@@ -0,0 +1,10 @@
+public class Krystian implements Uczen {
+
+    @Override
+    public void uczSie() {
+        System.out.println("Ucze sie bo Monika mnie zmusza");
+    }
+    public void ogladajKiepskich(){
+        System.out.println("Ogladam kiepskich w morde jeża");
+    }
+}
---
+++ src/Roch.java
@@ -0,0 +1,9 @@
+public class Roch implements Uczen{
+    @Override
+    public void uczSie() {
+        System.out.println("Bleeeeee duzo mowie i sie ucze");
+    }
+    public void grajWSzachy(){
+        System.out.println("Pokonałem Ernesta w szachy");
+    }
+}
